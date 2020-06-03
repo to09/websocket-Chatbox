1076f6985e7a99f96e03c84f32c05ff53d4935ec
Prajjwal kumar agrahari <45192178+to09@users.noreply.github.com>
2020-06-03 15:43:54 +0530

    add git repo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='websocket_chatbox',
     version='0.4',
     packages=find_packages("."),
-    url='https://github.com/to09,
+    url='https://github.com/to09/websocket-Chatbox',
     license='MIT',
     author='Prajjwal Kumar Agrahari',
     author_email='agraharitoshu854204@gmail.com',
